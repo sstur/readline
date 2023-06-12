7523ea8787b89d5a552bf0995c838c141dc8d4f5
Simon <sstur@me.com>
2023-06-12 07:58:12 -0700

    Add back in d.ts files

---
+++ sync/index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Present the user with a prompt to enter some text. This assumes stdin and
+ * stdout are connected to an interactive terminal (TTY).
+ */
+export function readline(prompt: string): string;
